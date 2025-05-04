4f8078b0a692cc0f3ee09ad855b51ecbd9581742
rhysrose <rose.funja@gmail.com>
2025-05-04 22:13:24 +0300

    app.py data_utils

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from pages import data_exploration, model_training, prediction_page
+from data_utils import load_data
+
+# Set the epage configurations
+st.set_page_config(
+    page_title= "Climate  Trend Predictor",
+    page_icon= '🌦️', 
+    layout= "wide"
+)
+
+# Give the Title and description
+st.title("Climate Trend Analysis and Prediction")
+st.markdown("Analyze the Historical Rainfall and Predict Future Trends")
+
+#load the data
+filepath = r"/Users/rosepeterfunja/Tanzania_KIC/Assignment/capstone-project-rhysrose/data/tanzania_climate_data.csv"
+df = load_data(filepath)
+
+#Check if data is loaded 
+st.write("Data Loaded")
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ['Data Exploration' ,'Model Training', 'Prediction'])
+
+#Display the selected Page
+if page == "Data Exploration":
+    data_exploration.show(df)
+elif page == "Model Training":
+    model_training.show(df)
+else:
+    prediction_page.show(df)
+
+
---
+++ data_utils.py
@@ -0,0 +1,38 @@
+#Import Libraries
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+#load the data
+filepath = r"/Users/rosepeterfunja/Tanzania_KIC/Assignment/capstone-project-rhysrose/data/tanzania_climate_data.csv"
+
+@st.cache_data
+def load_data(filepath):
+    """
+    Load the climate CSV data from the given filepath
+
+    Parameters:
+    filepath(str): Path to the CSV file
+
+    Returns:
+    pd.DataFrame: loaded and cached Dataframe
+    """
+
+    try:
+        data = pd.read_csv(filepath)
+        return data
+    except FileNotFoundError:
+        st.error(f"File not found: {filepath}")
+        return pd.DataFrame()
+    except Exception as e:
+        st.error(f"An error occured: {e}")
+        return pd.DataFrame()
+    
+
+def prepare_features(df):
+    """
+    Prepare the Features for model training
+    """
+    X = df[['Year','Month','Average_Temperature_C','Max_Temperature_C','Min_Temperature_C']].values
+    y = df[['Total_Rainfall_mm']].values
+    return X, y
